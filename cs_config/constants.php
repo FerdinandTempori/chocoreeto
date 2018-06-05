186f7b0c4ab43944146d347e9e01a2fdf3981fbf
FerdinandTempori <39939852+FerdinandTempori@users.noreply.github.com>
2018-06-05 01:01:37 -0500

    Delete constants.php

--- cs_config/constants.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$globalMessages=array();
-$partialScripts=array();
-$relURI = $_SERVER['HTTP_HOST']."/is/";
-$timeout=7200;
-$server = array("127.0.0.1", "chocolateshop", "root", "fernanda123", 3336);
-$serverApiKey='AAAAc8chhaY:APA91bFLSOZGvdtwnUZidEHJxF3U1I9613WsITNYMlGqcZE8R01LUM0V-9qQWH5w5jgCFvUVkyqFxUPhc374guRSaFdvXk0nzllbpF0tzssM8ca3dgAGksjIoNl21X2p4qSz9y_h2br5';
-$senderId='497262101926';
- ?>
